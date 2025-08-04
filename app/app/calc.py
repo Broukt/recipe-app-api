61bbe27cb5f8535e2eb4023923c5fcb6f651e55b
Jairo Calcina <j.calcina.v@gmail.com>
2025-08-03 22:11:13 -0400

    test: add simple tests

---
+++ app/app/calc.py
@@ -0,0 +1,31 @@
+"""
+Calculator functions
+"""
+
+
+def add(x: int, y: int) -> int:
+    """
+    Add two numbers x and y and return result.
+
+    Args:
+        x (int): First number to sum.
+        y (int): Second number to sum.
+
+    Returns:
+        int: Operation result.
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """
+    Subtract x from y and return result.
+
+    Args:
+        x (int): First number to subtract
+        y (int): Second number to subtract
+
+    Returns:
+        int: Operation result
+    """
+    return x - y
